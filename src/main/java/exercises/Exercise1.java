b9586bc46c68407877d2bb81ada28f1f764a8cd0
Victor Boleac <victor.boleac@icloud.com>
2024-10-30 12:30:33 +0000

    feat: task 6

--- src/main/java/exercises/Exercise1.java
+++ src/main/java/exercises/Exercise1.java
@@ -13,37 +13,49 @@ public class Exercise1 {
 
     public static void divideUserInputs() {
         while (true) {
-            try {
                 int[] userInputs = getUserInputs();
                 int result = divide(userInputs[0], userInputs[1]);
                 System.out.println(result);
                 break;
-            }catch (InputMismatchException e) {
-              System.out.println("Invalid input. Please try again.");
-            }
-            catch (ArithmeticException e) {
-                System.out.println("cannot divide by zero, please try again.");
-            }catch (NegativeIntegerInputException e){
-                System.out.println(e.getMessage());
             }
         }
-    }
 
-    private static int[] getUserInputs() throws NegativeIntegerInputException {
+
+    private static InputResult getUserInputs() throws NegativeIntegerInputException {
         Scanner userInputs = new Scanner(System.in);
-
+        InputResult inputResult = new InputResult();
         int[] inputs = new int[2];
-        System.out.print("Enter divisor: ");
-        inputs[0] = userInputs.nextInt();
-        System.out.print("Enter dividend: ");
-        inputs[1] = userInputs.nextInt();
-        if(inputs[0]<0 && inputs[1]<0){
-            throw new NegativeIntegerInputException(inputs[0], inputs[1]);
+        try {
+            System.out.print("Enter divisor: ");
+            inputs[0] = userInputs.nextInt();
+        } catch (InputMismatchException e){
+            inputResult.success = false;
+            inputResult.message = "Should be an integer";
+            return inputResult;
+            }
+        try {
+            System.out.print("Enter dividend: ");
+            inputs[1] = userInputs.nextInt();
+        }catch (InputMismatchException e) {
+            inputResult.success = false;
+            inputResult.message = "Should be an integer";
+            return inputResult;
         }
-        if(inputs[0]<0 || inputs[1]<0){
-            throw new NegativeIntegerInputException(inputs[0]<0? inputs[0]:inputs[1]);
+
+
+            if (inputs[1] == 0) {
+                throw new ArithmeticException("Cannot divide by zero, please try again.");
+            }
+            if (inputs[0] < 0 && inputs[1] < 0) {
+                throw new NegativeIntegerInputException(inputs[0], inputs[1]);
+            }
+            if (inputs[0] < 0 || inputs[1] < 0) {
+                throw new NegativeIntegerInputException(inputs[0] < 0 ? inputs[0] : inputs[1]);
+            }
+        }catch (Exception e){
+            return new InputResult();
         }
-        return inputs;
+        return new InputResult();
 
     }
 }
---
+++ src/main/java/exercises/InputResult.java
@@ -0,0 +1,8 @@
+package exercises;
+
+public class InputResult {
+    public boolean success;
+    public int[] result = new int[2];
+    public String message;
+
+}
